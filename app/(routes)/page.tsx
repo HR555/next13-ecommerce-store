82452811eb4c503814636a5da82533c0abc1e96d
HR555 <hasith.rc@gmail.com>
2023-12-09 15:18:19 +0000

    Ciyols Changes

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("0ad0c280-97ce-4381-9c84-8e1e12ea77d9");
 
   return (
     <Container>
